f8d734c31977013e3abe8b495b8ba7c2b5197fb5
mdiazpj <110750463+mndiazf@users.noreply.github.com>
2024-07-02 17:11:46 -0400

    ssl-commit

--- index.js
+++ index.js
@@ -4,21 +4,52 @@ import path from 'path';
 import cors from 'cors';
 import paymentRoutes from './routes/payment.routes.js';
 import { connectToDatabase } from './config/database.js';
+import fs from 'fs';
+import https from 'https';
 
+// Crea una instancia de Express para HTTPS
 const app = express();
 
+// Configura CORS para aceptar cualquier origen
 app.use(cors({
-  origin: 'https://mousecat.lol/',
+  origin: '*',  // Permite cualquier origen
   methods: ['GET', 'POST'],
 }));
 
+// Middlewares
 app.use(express.json());
 app.use(morgan('dev'));
+
+// Rutas
 app.use(paymentRoutes);
+
+// Archivos estáticos
 app.use(express.static(path.resolve('src/public')));
 
+// Conexión a la base de datos
 connectToDatabase().catch(err => console.error('Failed to connect to database', err));
 
-app.listen(3000, () => {
-  console.log('Server on port', 3000);
+// Opciones para HTTPS
+const httpsOptions = {
+  key: fs.readFileSync('./mousecat.xyz.key'), // Ruta al archivo de clave en la carpeta raíz
+  cert: fs.readFileSync('./mousecat.xyz.cert')  // Ruta al archivo de certificado en la carpeta raíz
+};
+
+// Crear servidor HTTPS en el puerto 8444
+https.createServer(httpsOptions, app).listen(8444, () => {
+  console.log('Servidor HTTPS corriendo en el puerto 8444');
+});
+
+// Crear una instancia de Express para HTTP
+const httpApp = express();
+httpApp.use((req, res, next) => {
+  if (!req.secure) {
+    return res.redirect('https://' + req.headers.host.replace(/:3000$/, ':8444') + req.url);
+  }
+  next();
+});
+
+// Escuchar HTTP en el puerto 3000 y redirigir a HTTPS
+httpApp.listen(3000, () => {
+  console.log('Servidor HTTP corriendo en el puerto 3000 y redirigiendo a HTTPS');
 });
